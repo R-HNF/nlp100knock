f00e8f74c168d240b20a7a449bd99671c96050eb
Ryo Hanafusa <ryo7gumi@gmail.com>
2015-11-28 03:15:25 +0900

    solved a encoding problem

--- 01.py
+++ 01.py
@@ -1,4 +1,7 @@
-#conding: utf-8
-str="patoka---"
+#-*-coding:utf-8-*-
+str=u"パトカーーー"
+#str="patoka---"
+
 #print "".join([str[2*i] for i in range(len(str)/2)])
+
 print str[::2]
--- 02.py
+++ 02.py
@@ -1,3 +1,4 @@
-str1="patoka-"
-str2="takusi-"
+#-*-coding:utf-8-*-
+str1=u"パトカー"
+str2=u"タクシー"
 print "".join([str1[i]+str2[i] for i in range(len(str1))])
