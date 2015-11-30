4a56f0e7da69ddc58e000105f14051c43c9d036b
ryo <ryo7gumi@gmail.com>
2015-11-30 18:04:45 +0900

    change 05.py

--- 05.py
+++ 05.py
@@ -1,10 +1,21 @@
 sentence="I am an NLPer"
+#sentence=["I","am","an","NLPer"]
 
 def ngram(sentence,n):
-    words=sentence.split()
-    print [words[i:i+n] for i in range(0,len(words),n)]
-    seq="".join(words)
-    print [seq[i:i+n] for i in range(0,len(seq),n)]
-    #print [sentence[i:i+n] for i in range(0,len(sentence),n)]
+    #change to str from list
+    if(isinstance(sentence,list)):sentence=" ".join(sentence)
 
-ngram(sentence,2)
+    #word n-gram
+    words=sentence.split()
+    wgram=[words[i:i+n] for i in range(len(words)-n+1)]
+
+    #character n-gram
+    cgram=[[sentence[i+j] for j in range(n)] for i in range(len(sentence)-n+1)]
+    #return n-grams
+    return wgram,cgram
+
+
+wgram,cgram=ngram(sentence,2)
+#wgram,cgram=ngram(sentence,3)
+print wgram
+print cgram
